1bd73d26e9a0bcf1618398d19331f0a8885ab0f9
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-24 18:50:43 -0700

    initial commit

---
+++ js/snake.js
@@ -0,0 +1,19 @@
+(function () {
+  if (typeof SnakeGame === 'undefined') {
+    window.SnakeGame = {};
+  }
+
+  var Snake = SnakeGame.Snake = function (board) {
+    this.direction = "N";
+    this.segments = [board.center()];
+  };
+
+  Snake.DIR = ["N", "S", "E", "W"];
+
+
+
+
+
+
+
+})
