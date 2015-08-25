20782879bfa5f28f51c90e0fde5ccf228742f9b1
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-24 19:09:25 -0700

    added snake#move

--- js/snake.js
+++ js/snake.js
@@ -5,10 +5,31 @@
 
   var Snake = SnakeGame.Snake = function (board) {
     this.direction = "N";
-    this.segments = [board.center()];
+    var centerX = board.centerX();
+    var centerY = board.centerY();
+    this.segments = [new Coord(centerX, centerY)];
   };
 
   Snake.DIR = ["N", "S", "E", "W"];
+
+  Snake.MOVEMENTS = {
+    "N": new Coord(-1, 0),
+    "S": new Coord(1, 0),
+    "W": new Coord(0, -1),
+    "E": new Coord(0, 1),
+  };
+
+  Snake.prototype.head = function() {
+    return this.segments[0];
+  };
+
+  Snake.prototype.move = function() {
+    //head gets new location
+    this.segments.unshift(this.head().plus(Snake.MOVEMENTS[this.dir]));
+
+    //tail gets cut off cause moved forward
+    this.segments.pop();
+  };
 
 
 
